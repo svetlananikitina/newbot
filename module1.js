9a9c781b5bfcea3861bd614570f9349222cd6761
svetlananikitina <nikitinasvetlana92@gmail.com>
2017-06-15 13:54:51 +0200

    html added

--- module1.js
+++ module1.js
@@ -1,3 +1,26 @@
 /**
  * Created by svetlananikitina on 15/06/2017.
  */
+
+
+//module that returns the current date and time
+
+// exports.myDateTime = function () {
+//     return Date();
+// };
+
+var fruits = ['Banana','Apple','Orange','Mango'];
+
+//print array of fruits
+function loadFruits() {
+    document.getElementById("fruits").innerHTML = fruits;
+}
+
+//print fruit
+function myFunction() {
+    var fruit = prompt("What is your favorite fruit?");
+    fruits[fruits.length]= fruit;
+    document.getElementById("fruits").innerHTML = fruits;
+
+}
+
--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 var http = require('http');
+var dt = require('./module1');
 
+
+const express = require('express');
+const app = express();
+
+const myport = process.env.PORT || 5000;
+
+//create a server object:
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-}).listen(process.env.PORT || 5000);
+    res.writeHead(200, {'Content-Type': 'text/html'}); //status code, response headers
+    res.write("Date and time are currently: " + dt.myDateTime());
+    res.end('Hello bot!'); // end of response
+}).listen(myport); //the server object listens on port myport
+
+
+// app.get('/', function (req, res) {
+//     res.send('Hello I am chat bot')
+//
+// })
+
+
+
+app.listen(3000, function () {
+    console.log('Example app listening on port!'+myport, process.env.MSG)
+});
+
