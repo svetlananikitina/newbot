5892241d6f9c2baa11b24c57d428486ae0842300
svetlananikitina <nikitinasvetlana92@gmail.com>
2017-06-19 18:20:23 +0200

    fetch

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 // var dt = require('./module1');
 var fetch = require('node-fetch');
 
-var http = require('http').Server(app);
+// var http = require('http').Server(app);
 var request = require('request');
 
 // require('es6-promise').polyfill();
@@ -13,11 +13,11 @@ const app = express();
 const myport = process.env.PORT || 5000;
 
 //create a server object:
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'}); //status code, response headers
-    res.write("Date and time are currently: ");
-    res.end('Hello bot!'); // end of response
-}).listen(myport); //the server object listens on port myport
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'}); //status code, response headers
+//     res.write("Date and time are currently: ");
+//     res.end('Hello bot!'); // end of response
+// }).listen(myport); //the server object listens on port myport
 
 //Respond with Hello World! when a GET request is made to the homepage
 
@@ -37,7 +37,7 @@ app.get('/', function (req, res) {
 })
 
 
-app.listen(3000, function () {
+app.listen(myport, function () {
     console.log('Example app listening on port!'+myport, process.env.MSG)
 });
 
